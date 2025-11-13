b927c0f5819f22807b216992c83dee908259c6ba
aleksanderZak7 <aleksander.zak77@gmail.com>
2025-11-13 15:50:19 +0100

    feat: Adding base interfaces

---
+++ Collectify.Model/Interfaces/ICollectionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Collectify.Model.Entities;
+
+namespace Collectify.Model.Interfaces;
+
+/// <summary>
+/// Repository interface dedicated to <see cref="Collection"/> entities.
+/// </summary>
+public interface ICollectionRepository : IRepository<Collection>
+{
+    /// <summary>
+    /// Asynchronously retrieves all collections including their items.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>
+    /// Task that results in a read only list of collections with items.
+    /// </returns>
+    Task<IReadOnlyList<Collection>> GetWithItemsAsync(CancellationToken cancellationToken = default);
+}
