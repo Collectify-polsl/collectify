22c9f08e5bd0fc99cf1df363cd1f66ee0f1266c4
casper9474 <casper94749474@gmail.com>
2025-11-16 19:07:32 +0100

    fix: remove database logic from ItemService

--- Collectify.Data/ItemRepository.cs
+++ Collectify.Data/ItemRepository.cs
@@ -1,13 +1,9 @@
-﻿namespace Collectify.Data;
-
-using Collectify.Model.Collection;
+﻿using Collectify.Model.Collection;
 using Collectify.Model.Entities;
 using System.Linq;
 
-public class ItemService(CollectifyContext context)
+public class ItemService
 {
-    private readonly CollectifyContext _context = context;
-
     public void SetFieldValue(Item item, FieldDefinition fieldDef, object? value)
     {
         if (fieldDef.IsList)
@@ -15,20 +11,10 @@ public class ItemService(CollectifyContext context)
             var newFieldValue = new FieldValue
             {
                 Item = item,
-                ItemId = item.Id,
                 FieldDefinition = fieldDef,
-                FieldDefinitionId = fieldDef.Id
             };
             newFieldValue.SetValue(value);
-
-            if (item.Id > 0)
-            {
-                _context.Values.Add(newFieldValue);
-            }
-            else
-            {
-                item.FieldValues.Add(newFieldValue);
-            }
+            item.FieldValues.Add(newFieldValue);
         }
         else
         {
@@ -40,19 +26,9 @@ public class ItemService(CollectifyContext context)
                 fieldValue = new FieldValue
                 {
                     Item = item,
-                    ItemId = item.Id,
                     FieldDefinition = fieldDef,
-                    FieldDefinitionId = fieldDef.Id
                 };
-
-                if (item.Id > 0)
-                {
-                    _context.Values.Add(fieldValue);
-                }
-                else
-                {
-                    item.FieldValues.Add(fieldValue);
-                }
+                item.FieldValues.Add(fieldValue);
             }
 
             fieldValue.SetValue(value);
@@ -76,7 +52,6 @@ public class ItemService(CollectifyContext context)
 
         if (fieldValueToRemove != null)
         {
-            _context.Values.Remove(fieldValueToRemove);
             item.FieldValues.Remove(fieldValueToRemove);
             return true;
         }
@@ -86,13 +61,6 @@ public class ItemService(CollectifyContext context)
 
     public List<object?> GetFieldValues(Item item, FieldDefinition fieldDef)
     {
-        if (!item.FieldValues.Any() && item.Id > 0)
-        {
-            _context.Entry(item)
-               .Collection(x => x.FieldValues)
-               .Load();
-        }
-
         return item.FieldValues
             .Where(x => x.FieldDefinitionId == fieldDef.Id)
             .Select(x => x.GetValue())
