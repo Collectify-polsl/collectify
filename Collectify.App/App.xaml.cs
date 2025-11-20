77be4d285c07fde42b7f59f09fb2b9c8c74e4ce7
aleksanderZak7 <aleksander.zak77@gmail.com>
2025-11-20 13:52:35 +0100

    feat: Wire WPF startup with MainWindowViewModel and EfUnitOfWork

--- Collectify.App/App.xaml.cs
+++ Collectify.App/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Collectify.App.ViewModels;
+using Collectify.Data;
+using Collectify.Model.Interfaces;
+using System.Windows;
 
 namespace Collectify.App
 {
@@ -7,6 +10,20 @@ namespace Collectify.App
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            CollectifyContext context = DbFactory.CreateContext();
+            IUnitOfWork unitOfWork = new EfUnitOfWork(context);
+
+            MainWindowViewModel mainViewModel = new MainWindowViewModel(unitOfWork);
+
+            MainWindow mainWindow = new MainWindow
+            {
+                DataContext = mainViewModel
+            };
+            mainWindow.Show();
+        }
+    }
 }
---
+++ Collectify.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Collectify.App.Commands;
+using Collectify.Model.Collection;
+using Collectify.Model.Interfaces;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+
+namespace Collectify.App.ViewModels;
+
+public class MainWindowViewModel
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ObservableCollection<Collection> Collections { get; } = new();
+
+    public ICommand LoadCollectionsCommand { get; }
+
+    public MainWindowViewModel(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        LoadCollectionsCommand = new AsyncRelayCommand(LoadCollectionsAsync);
+    }
+
+    private async Task LoadCollectionsAsync()
+    {
+        Collections.Clear();
+        var collections = await _unitOfWork.Collections.GetAllAsync();
+        foreach (var c in collections)
+            Collections.Add(c);
+    }
+}
